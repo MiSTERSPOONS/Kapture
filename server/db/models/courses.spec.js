e249ff40d4d1fc5d80e6f8a06dfcb9c288258265
Jonathan Ahn <jonathanahnmusic@gmail.com>
2017-11-16 17:24:44 -0500

    finish defining Course Model and writing specs for courses

---
+++ server/db/models/courses.spec.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const db = require('../db');
+
+const Course = db.model('course');
+
+describe('Course model', () => {
+
+  beforeEach( () => db.sync({ force: true }));
+
+  describe('Create Course', () => {
+
+    let testCourse;
+
+    beforeEach( () => Course.create({
+      name: 'JavaScript 9000'
+    })
+    .then( course => {
+      testCourse = course;
+    }));
+
+    it('returns a name', () => {
+      expect(testCourse.name).to.be.equal('JavaScript 9000');
+    })
+  })
+})
