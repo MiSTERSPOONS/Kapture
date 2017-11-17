0afb2c66a2ea325c2619579e63cd4ca34d44bdb3
Jonathan Chang <jonathanmchang@gmail.com>
2017-11-17 12:39:22 -0500

    got react component to render and setup react-redux store for later use

---
+++ client/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+  constructor() {
+    super()
+
+  }
+  render() {
+    return (
+      <div>
+      <h1>hi jon, i'm smoking</h1>
+      </div>
+    )
+  }
+}
--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Main } from './components';
+// import store from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Main />
-  </Provider>,
+  <Main />,
   document.getElementById('app')
 )
