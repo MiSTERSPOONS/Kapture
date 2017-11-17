cc09f855fb04349a7b8e3dd9442af934c65c0cf4
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-11-17 16:27:06 -0500

    Add association tests to instuctor, student, cohort

--- server/db/models/instructors.spec.js
+++ server/db/models/instructors.spec.js
@@ -2,53 +2,58 @@ const { expect } = require('chai');
 const db = require('../../db');
 
 const Instructor = db.model('instructor');
-const Student = db.model('student');
+
+beforeEach( () => db.sync({ force: true }));
 
 describe('Instructor model', () => {
+  let testInstructor;
 
-  beforeEach( () => db.sync({ force: true }));
+  beforeEach( () => Instructor.create({
+    firstName: 'InstFirst',
+    lastName: 'InstLast',
+    email: 'inst@example.com',
+    password: 'instPw',
+    cohorts: [{
+      name: 'InstCohort'
+    }],
+    courses: [{
+      name: 'InstCourse'
+    }],
+    students: [{
+      firstName: 'InstStudentFirst',
+      lastName: 'InstStudentLast',
+      email: 'instStudent@example.com',
+      password: 'instStudentPw',
+    }]
+  }, { include: [{ all: true }]} )
+  .then( instructor => {
+    testInstructor = instructor;
+  }));
 
   describe('Create Instructor', () => {
-
-    let testInstructor;
-    let testStudent;
-
-    beforeEach( () => Instructor.create({
-      firstName: 'Corey',
-      lastName: 'Greenwald',
-      email: 'corey@greenwald.com',
-      password: 'smite',
-    })
-    .then( instructor => {
-      testInstructor = instructor;
-      return Student.create({
-        firstName: 'StudentFirst',
-        lastName: 'StudentLast',
-        email: 'student@example.com',
-        password: 'password',
-      })
-    }).then( student => {
-      testStudent = student;
-      return testInstructor.setStudents([testStudent]);
-    }));
-
     it('returns a firstName', () => {
-      expect(testInstructor.firstName).to.be.equal('Corey');
-    })
+      expect(testInstructor.firstName).to.be.equal('InstFirst');
+    });
     it('returns a lastName', () => {
-      expect(testInstructor.lastName).to.be.equal('Greenwald');
-    })
+      expect(testInstructor.lastName).to.be.equal('InstLast');
+    });
     it('returns an email', () => {
-      expect(testInstructor.email).to.be.equal('corey@greenwald.com');
-    })
+      expect(testInstructor.email).to.be.equal('inst@example.com');
+    });
     it('returns a password', () => {
-      expect(testInstructor.password).to.be.equal('smite');
-    })
-    it('returns an instructor\'s student', () => {
-      testInstructor.getStudents()
-      .then( students => {
-        expect(students[0].firstName).to.be.equal('StudentFirst');
-      })
-    })
-  })
-})
+      expect(testInstructor.password).to.be.equal('instPw');
+    });
+  });
+
+  describe('Instructor Associations', () => {
+    it(`Returns an instructor's cohorts`, () => {
+      expect(testInstructor.cohorts[0].name).to.be.equal('InstCohort');
+    });
+    it(`Returns an instructor's courses`, () => {
+      expect(testInstructor.courses[0].name).to.be.equal('InstCourse');
+    });
+    it(`Returns an instructor's students`, () => {
+      expect(testInstructor.students[0].firstName).to.be.equal('InstStudentFirst');
+    });
+  });
+});
--- server/db/models/students.spec.js
+++ server/db/models/students.spec.js
@@ -2,54 +2,58 @@ const { expect } = require('chai');
 const db = require('../../db');
 
 const Student = db.model('student');
-const Instructor = db.model('instructor');
 
-describe('Student model', () => {
+beforeEach( () => db.sync({ force: true }));
 
-  beforeEach( () => db.sync({ force: true }));
+describe('Student Model', () => {
+  let testStudent;
 
-  describe('Create Student', () => {
+  beforeEach( () => Student.create({
+    firstName: 'StudentFirst',
+    lastName: 'StudentLast',
+    email: 'student@example.com',
+    password: 'studentPw',
+    instructors: [{ // Multiple Instructors
+      firstName: 'InstFirst',
+      lastName: 'InstLast',
+      email: 'inst@example.com',
+      password: 'instPw',
+    }],
+    courses: [{ // Multiple Courses
+      name: 'StudentCourse'
+    }],
+    cohort: { // Single Cohort
+      name: 'StudentCohort'
+    }
+  }, { include: [{ all: true }]} )
+  .then( student => {
+    testStudent = student;
+  }));
 
-    let testStudent;
-    let testInstructor;
-
-    beforeEach( () => Student.create({
-      firstName: 'StudentFirst',
-      lastName: 'StudentLast',
-      email: 'student@example.com',
-      password: 'password',
-    })
-    .then( student => {
-      testStudent = student;
-      return Instructor.create({
-        firstName: 'Inst1First',
-        lastName: 'Inst1Last',
-        email: 'inst1@example.com',
-        password: 'pw1',
-      })
-    })
-    .then( instructor => {
-      testInstructor = instructor;
-      return testStudent.setInstructors([testInstructor]);
-    }));
-
+  describe('Student Creation', () => {
     it('returns a firstName', () => {
       expect(testStudent.firstName).to.be.equal('StudentFirst');
-    })
+    });
     it('returns a lastName', () => {
       expect(testStudent.lastName).to.be.equal('StudentLast');
-    })
+    });
     it('returns an email', () => {
       expect(testStudent.email).to.be.equal('student@example.com');
-    })
+    });
     it('returns a password', () => {
-      expect(testStudent.password).to.be.equal('password');
-    })
-    it('returns a student\'s instructor', () => {
-      testStudent.getInstructors()
-      .then( instructors => {
-        expect(instructors[0].firstName).to.be.equal('Inst1First');
-      })
-    })
-  })
-})
+      expect(testStudent.password).to.be.equal('studentPw');
+    });
+  });
+
+  describe('Student Associations', () => {
+    it(`Returns a student's instructor`, () => {
+      expect(testStudent.instructors[0].firstName).to.be.equal('InstFirst');
+    });
+    it(`Returns a student's courses`, () => {
+      expect(testStudent.courses[0].name).to.be.equal('StudentCourse');
+    });
+    it(`Returns a student's cohort`, () => {
+      expect(testStudent.cohort.name).to.be.equal('StudentCohort');
+    });
+  });
+});
