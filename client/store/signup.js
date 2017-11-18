c3a11ce40ba683f00e55d52d4770d0ceab36e369
Jonathan Chang <jonathanmchang@gmail.com>
2017-11-18 13:45:47 -0500

    signup thunk now history.pushes to snapshot component

--- client/store/signup.js
+++ client/store/signup.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import history from '../history';
 
 // Action Type
 
@@ -6,21 +7,22 @@ const STUDENT_SIGNUP = 'STUDENT_SIGNUP';
 
 // Action Creators
 
-const studentSignup = (studentInfo) => ({ type: STUDENT_SIGNUP, studentInfo });
+const studentSignup = studentInfo => ({ type: STUDENT_SIGNUP, studentInfo });
 
 // THUNK
 
-export const submitStudentSignup = (studentInfo) => (dispatch) => {
+export const submitStudentSignup = studentInfo => (dispatch) => {
   axios.post('/api/students', studentInfo)
-  .then( postedStudent => dispatch(studentSignup(postedStudent)))
-  .catch(err => console.error(err))
+    .then(postedStudent => dispatch(studentSignup(postedStudent)))
+    .then(() => history.push('/snapshot'))
+    .catch(err => console.error(err));
 };
 
-export default ( state = {}, action) => {
-  switch(action.type) {
+export default (state = {}, action) => {
+  switch (action.type) {
     case STUDENT_SIGNUP:
-      return action.studentInfo
+      return action.studentInfo;
     default:
-      return state
+      return state;
   }
 };
