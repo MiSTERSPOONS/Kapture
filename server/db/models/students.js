d02afccd7cec1fb19256ac8784735bdf9fbc9438
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-11-18 14:20:24 -0500

    Update student model with hash, salt for password

--- server/db/models/students.js
+++ server/db/models/students.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const Sequelize = require('sequelize');
+const crypto = require('crypto');
 
 const Student = db.define('student', {
   firstName: {
@@ -17,7 +18,36 @@ const Student = db.define('student', {
   },
   password: {
     type: Sequelize.STRING
+  },
+  salt: {
+    type: Sequelize.STRING
   }
-})
+});
 
 module.exports = Student
+
+Student.prototype.correctPassword = function(candidatePwd) {
+  return Student.encryptPassword(candidatePwd, this.salt) === this.password;
+};
+
+Student.encryptPassword = function(plainText, salt) {
+  return crypto
+  .createHash('RSA-SHA256')
+  .update(plainText)
+  .update(salt)
+  .digest('hex');
+};
+
+Student.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+};
+
+const setSaltAndPassword = function(student) {
+  if (student.changed('password')) {
+    student.salt = Student.generateSalt();
+    student.password = Student.encryptPassword(student.password, student.salt);
+  }
+};
+
+Student.beforeCreate(setSaltAndPassword);
+Student.beforeUpdate(setSaltAndPassword);
