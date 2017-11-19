fe6f4e9355e0bd90b970ea9ab64020cf9e1ce4c4
Jonathan Chang <jonathanmchang@gmail.com>
2017-11-19 13:55:29 -0500

    cleaned up code in snapshot component

--- client/components/Snapshot.js
+++ client/components/Snapshot.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import api from '../../api_secrets.js';
 import Webcam from 'react-webcam';
 import { withRouter } from 'react-router-dom';
 import { enrollKairosCapture } from '../store/signup';
@@ -9,31 +8,17 @@ class Snapshot extends Component {
   constructor(props) {
     super(props);
     this.setRef = this.setRef.bind(this);
-    // this.capture = this.capture.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    this.capture = this.capture.bind(this);
   }
 
   setRef(webcam) {
     this.webcam = webcam;
   }
 
-  handleClick(event, id) {
+  capture() {
     const imageSrc = this.webcam.getScreenshot().slice(22);
-    this.props.capture(imageSrc, id)
+    this.props.sendCapture(imageSrc, this.props.studentInfo.id)
   }
-
-  // capture() {
-  //   const imageSrc = this.webcam.getScreenshot().slice(22);
-  //   // const kairos = new Kairos(api["api_id"], api["api_key"]);
-  //   // const { id } = this.props.studentInfo;
-  //   // kairos.enroll(imageSrc, "test", id, (response) => {
-  //   //   console.log(JSON.parse(response.responseText));
-  //   // });
-
-  //   // kairos.verify(imageSrc, 'test', '1', (response) => {
-  //   //   console.log(JSON.parse(response.responseText).images[0].transaction.confidence);
-  //   // });
-  // }
 
   render() {
     console.log("This is this.props! ", this.props)
@@ -46,7 +31,7 @@ class Snapshot extends Component {
           screenshotFormat="image/png"
           width={350}
         />
-        <button onClick={(event) => this.handleClick(event, this.props.studentInfo.id)}>Capture photo</button>
+        <button onClick={this.capture}>Capture photo</button>
       </div>
     );
   }
@@ -58,7 +43,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    capture: (imageSrc, id) => {
+    sendCapture: (imageSrc, id) => {
       const who = ownProps.match.url.slice(1)
       dispatch(enrollKairosCapture(imageSrc, who, id));
     }
