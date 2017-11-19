31d6e89435ab434d4f17cca60f97ec25ad2c0a23
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-11-19 16:27:59 -0500

    Add snapshotType to redux store

---
+++ client/store/snapshotType.js
@@ -0,0 +1,16 @@
+// Action Type
+
+const SIGNUP_SNAPSHOT = 'SIGNUP_SNAPSHOT';
+
+// Action Creators
+
+export const snapshotType = typeString => ({type: SIGNUP_SNAPSHOT, typeString });
+
+export default (state = '', action) => {
+  switch (action.type) {
+    case SIGNUP_SNAPSHOT:
+      return action.typeString;
+    default:
+      return state;
+  }
+};
