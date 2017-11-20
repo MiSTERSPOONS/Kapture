f8568fd18714aa546cc02eff8a65aa0f1b322897
Jonathan Chang <jonathanmchang@gmail.com>
2017-11-20 17:38:16 -0500

    created tables on studentDasboard to represent emotional data

--- client/components/StudentDashboard.js
+++ client/components/StudentDashboard.js
@@ -10,11 +10,42 @@ class StudentDashboard extends Component {
   render() {
     return (
       <div>
-        <h1>KINDA LOGGED IN!!!</h1>
-        {/*need to map*/}
-        {/*<tr key={emotion.id}>
-
-        </tr>*/}
+        <h1>Hello, {this.props.currentStudent.firstName}!</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Anger</th>
+              <th>Contempt</th>
+              <th>Disgust</th>
+              <th>Fear</th>
+              <th>Happiness</th>
+              <th>Neutral</th>
+              <th>Sadness</th>
+              <th>Surprise</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            this.props.currentStudent.emotions ?
+              this.props.currentStudent.emotions.map(emoData => {
+                return (
+                <tr key={emoData.id}>
+                  <td>{emoData.anger}</td>
+                  <td>{emoData.contempt}</td>
+                  <td>{emoData.disgust}</td>
+                  <td>{emoData.fear}</td>
+                  <td>{emoData.happiness}</td>
+                  <td>{emoData.neutral}</td>
+                  <td>{emoData.sadness}</td>
+                  <td>{emoData.surprise}</td>
+                </tr>
+                )
+              })
+              :
+              null
+          }
+          </tbody>
+        </table>
       </div>
     )
   }
