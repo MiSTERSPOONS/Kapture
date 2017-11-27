e7bd0f89580c8b6172f76490d5e4854437ceaae6
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-11-27 18:49:58 -0500

    Add dotenv to replace secrets

--- server/api/azure.js
+++ server/api/azure.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const axios = require('axios');
 
-const Emotion  = require('../db/models/emotions')
-const api = require('../../secrets')
+const Emotion  = require('../db/models/emotions');
 
 module.exports = router
 
@@ -10,7 +9,7 @@ router.post('/recognize', (req, res, next) => {
   axios.post('https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize', { "url": req.body.info.imageURL }, {
     headers: {
       'Content-type': "application/json",
-      'Ocp-Apim-Subscription-Key': process.env.AZURE_KEY || api.azureKey
+      'Ocp-Apim-Subscription-Key': process.env.AZURE_KEY
     }
   })
   .then(response => {
