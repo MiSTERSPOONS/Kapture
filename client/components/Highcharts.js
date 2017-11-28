de234f87943d5ac0200567a2e185744a960580c9
Jonathan Chang <jonathanmchang@gmail.com>
2017-11-28 16:16:47 -0500

    cleaned up graph and got rid of kapture button on student dash

--- client/components/Highcharts.js
+++ client/components/Highcharts.js
@@ -1,16 +1,17 @@
 const Highcharts = require('highcharts');
+const moment = require('moment');
 
 require('highcharts/modules/exporting')(Highcharts);
 
-const moment = require('moment');
+const characteristics = ['happiness', 'sadness', 'neutral', 'surprise', 'anger', 'fear', 'contempt', 'disgust', 'normalizeDate'];
 
-const characteristics = ['anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise', 'normalizeDate'];
+const colors = ['#8bbc21', '#8B436C', '#2f7ed8', '#E0E000', '#E10000', 
+'#20009A', '#000000', '#E06500', '#E06500', '#a6c96a']
 
 const normalizeData = (data, type) => {
   return data.map((inst) => {
       inst = Object.assign({}, inst, {normalizeDate: moment(inst.createdAt).format("MMM, D h:mma"
     )})
-    console.log('inst: ', inst)
     return inst[type]
   })
 }
@@ -19,78 +20,127 @@ const makeHighchart = (data) => {
     data.sort( (a, b) => {
         return a.id - b.id;
     })
-    console.log('dataaaa in HighCharts', data)
-
-  Highcharts.chart('container', {
-    chart: {
-        type: 'areaspline',
-    },
-    title: {
-        text: 'Emotions'
-    },
-    subtitle: {
-        text: 'Emotional score over time'
-    },
-    xAxis: {
-        type: characteristics[8],
-        labels: {
-            overflow: 'justify'
+for(let i = 0; i < 8; i++) {
+    Highcharts.chart(`container${i}`, {
+        chart: {
+            type: 'areaspline',
         },
-        categories: normalizeData(data, characteristics[8])
-    },
-    yAxis: {
         title: {
-            text: 'Likely emotion'
+            text: characteristics[i]
         },
-        minorGridLineWidth: 0,
-        gridLineWidth: 0,
-        alternateGridColor: null,
-        tickInterval: .05,
-        valueSuffix: ' %'
-
-    },
-    tooltip: {
-        valueSuffix: ' %',
-        crosshairs: [true, false]
-    },
-    plotOptions: {
-        areaspline: {
-            fillOpacity: 0.5
-        }
-    },
-    series: [{
-      name: characteristics[0],
-      data: normalizeData(data, characteristics[0])
-    }, {
-      name: characteristics[1],
-      data: normalizeData(data, characteristics[1])
-    }, {
-      name: characteristics[2],
-      data: normalizeData(data, characteristics[2])
-    }, {
-      name: characteristics[3],
-      data: normalizeData(data, characteristics[3])
-    }, {
-      name: characteristics[4],
-      data: normalizeData(data, characteristics[4])
-    }
-    // , {
-    //   name: characteristics[5],
-    //   data: normalizeData(data, characteristics[5])
-    // }
-    , {
-      name: characteristics[6],
-      data: normalizeData(data, characteristics[6])
-    }, {
-      name: characteristics[7],
-      data: normalizeData(data, characteristics[7])
-    }],
-      navigation: {
-          menuItemStyle: {
-              fontSize: '10px'
+        subtitle: {
+            text: 'Emotional score over time'
+        },
+        xAxis: {
+            type: characteristics[8],
+            labels: {
+                overflow: 'justify'
+            },
+            categories: normalizeData(data, characteristics[8])
+        },
+        yAxis: {
+            title: {
+                text: 'Likely emotion'
+            },
+            minorGridLineWidth: 0,
+            gridLineWidth: 0,
+            alternateGridColor: null,
+            tickInterval: .05,
+    
+        },
+        tooltip: {
+            valueSuffix: ' %',
+            crosshairs: [true, false]
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series: [{
+          name: characteristics[i],
+          data: normalizeData(data, characteristics[i]),
+          color: colors[i]
+        }],
+          navigation: {
+              menuItemStyle: {
+                  fontSize: '10px'
+              }
           }
-      }
-});
+    });
+}
+  
+
 }
 
 export default makeHighchart
+
+// Highcharts.chart('container1', {
+//     chart: {
+//         type: 'areaspline',
+//     },
+//     title: {
+//         text: 'Emotions'
+//     },
+//     subtitle: {
+//         text: 'Emotional score over time'
+//     },
+//     xAxis: {
+//         type: characteristics[8],
+//         labels: {
+//             overflow: 'justify'
+//         },
+//         categories: normalizeData(data, characteristics[8])
+//     },
+//     yAxis: {
+//         title: {
+//             text: 'Likely emotion'
+//         },
+//         minorGridLineWidth: 0,
+//         gridLineWidth: 0,
+//         alternateGridColor: null,
+//         tickInterval: .05,
+
+//     },
+//     tooltip: {
+//         valueSuffix: ' %',
+//         crosshairs: [true, false]
+//     },
+//     plotOptions: {
+//         areaspline: {
+//             fillOpacity: 0.5
+//         }
+//     },
+//     series: [{
+//       name: characteristics[0],
+//       data: normalizeData(data, characteristics[0])
+//     }, {
+//       name: characteristics[1],
+//       data: normalizeData(data, characteristics[1])
+//     }, {
+//       name: characteristics[2],
+//       data: normalizeData(data, characteristics[2])
+//     }, {
+//       name: characteristics[3],
+//       data: normalizeData(data, characteristics[3])
+//     }, {
+//       name: characteristics[4],
+//       data: normalizeData(data, characteristics[4])
+//     }
+//     // , {
+//     //   name: characteristics[5],
+//     //   data: normalizeData(data, characteristics[5])
+//     // }
+//     , {
+//       name: characteristics[6],
+//       data: normalizeData(data, characteristics[6])
+//     }, {
+//       name: characteristics[7],
+//       data: normalizeData(data, characteristics[7])
+//     }],
+//       navigation: {
+//           menuItemStyle: {
+//               fontSize: '10px'
+//           }
+//       }
+// });
--- server/auth/index.js
+++ server/auth/index.js
@@ -4,21 +4,16 @@ const Instructor = require('../db').model('instructor');
 module.exports = router
 
 router.get('/me', (req, res, next) => {
-  console.log("REQ DOT USER", req.user);
   if (req.user instanceof Student) {
-    // console.log('INSIDE INSTANCE OF STUDENT')
-    // res.redirect(`/students/${req.user.id}`)
     res.json({ userType: "students", user: req.user })
   } else if (req.user instanceof Instructor) {
-    // console.log('INSIDE INSTANCE OF INSTRUCTOR')
     res.json({ userType: "instructors", user: req.user })
   } else {
-    res.json({ message: 'No User' })
+    res.json({ userType: 'guest', user: {} })
   }
 })
 
 router.post('/login', (req, res, next) => {
-  console.log(req.body);
   const email = req.body.email
   const password = req.body.password
   const userType = req.body.userType
